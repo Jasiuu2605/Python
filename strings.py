8e88eeb2c4472aa5dd6cecb31b9bf298e2604769
Jasiuu2605 <98971547+Jasiuu2605@users.noreply.github.com>
2023-03-06 16:03:28 +0100

    practicing format method

---
+++ strings.py
@@ -0,0 +1,20 @@
+my_list = ['h', 'e', 'l', 'l', 'o']
+my_text = 'hello'
+
+my_list[0] = 'c'
+print(my_list)
+
+name = 'Jan'
+age = 30
+
+text = 'Jestem ' + name + ' i mam ' + str(age) + ' lat.'
+print(text)
+
+text_2 = 'Jestem {} i mam {} lat'.format(name, age)
+print(text_2)
+
+text_3 = 'I am {name} and I am {years} old'.format(name=name, years=age)
+
+funds = 150.9723
+
+print('Funds: {:10.1f}'.format(funds))
